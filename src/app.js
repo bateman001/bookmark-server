0e05fbbd5b3c22dc177ad2176652a17347643575
Your name here <kr.shampa@yahoo.com>
2020-07-09 16:11:20 -0500

    Adding Authorization

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const bookmarkRouter = require('./bookmark/bookmark-router')
 const app = express()
 const { NODE_ENV } = require('./config')
+const validateBearerToken =require('./validateBearerToken')
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -14,11 +15,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
-
-app.get('/', (req,res) => {
-    res.send("Hello, world!")
-})
-
+app.use(validateBearerToken)
 app.use(bookmarkRouter);
 
 app.use(function errorHandler(error, req, res, next) {
